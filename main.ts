d2f5f8898105e79108ee0afb9a88521e99ba6342
eazton <70408017+eazton@users.noreply.github.com>
2021-06-03 10:58:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,13 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("10987654321 GO GO GO GO GO ")
     basic.showString("10987654321 GO GO GO GO GO ")
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    radio.sendString("SET UP")
-    basic.showString("SET UP")
-    basic.clearScreen()
-})
 radio.onReceivedString(function (receivedString) {
     basic.clearScreen()
     basic.showString(receivedString)
 })
-input.onButtonPressed(Button.B, function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    radio.sendString("SET UP")
+    basic.showString("SET UP")
+    basic.clearScreen()
 })
